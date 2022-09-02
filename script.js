66866e5da295267a5966c284f43678f3ed0154c6
Chii Yuen <chii.yuen@hotmail.com>
2022-09-03 03:03:36 +0800

    Complete styling

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const rgbPalette = ['#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800', '#FF5722', '#795548', '#9E9E9E', '#607D8B'];
 let mode = "normal", size = 16, currentColor = '#000000', mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true)
+document.body.onmousedown = (e) => {
+  mouseDown = true;
+  e.preventDefault();}
 document.body.onmouseup = () => (mouseDown = false)
 
 function generateGrid() {
@@ -69,27 +71,30 @@ function cursor() {
   area.forEach(box => {
     box.counter = 0;
     box.addEventListener('mouseover', (e) => {
-      if (mouseDown) {
-        switch (mode) {
-          case 'erase':
-            e.target.style.backgroundColor = '#ffffff';
-            e.target.style.opacity = 1;
-            break;
-          case 'normal':
-            currentColor = e.target.style.backgroundColor = currentColor;
-            break;
-          case 'fill':
-            area.forEach(box => box.style.backgroundColor = currentColor);
-            break;
-          case 'rgb':
-            e.target.style.backgroundColor = rgbPalette[Math.round(Math.random() * rgbPalette.length)];
-            break;
-          case 'shade':
-            currentColor = '#000000';
-            box.counter += 1;
-            e.target.style.backgroundColor = currentColor;
-            e.target.style.opacity = 0.025 * box.counter;
-        }
+      if (!mouseDown) return;
+      switch (mode) {
+        case 'erase':
+          e.target.style.backgroundColor = '#ffffff';
+          e.target.style.opacity = 1;
+          break;
+        case 'normal':
+          e.target.style.backgroundColor = currentColor;
+          e.target.style.opacity = 1;
+          break;
+        case 'fill':
+          area.forEach(box => {
+            box.style.backgroundColor = currentColor;
+            box.style.opacity = 1;
+          });
+          break;
+        case 'rgb':
+          e.target.style.backgroundColor = rgbPalette[Math.round(Math.random() * rgbPalette.length)];
+          break;
+        case 'shade':
+          box.counter += 1;
+          e.target.style.backgroundColor = currentColor;
+          e.target.style.opacity = 0.02 * box.counter;
+          break;
       }
     })
   })
