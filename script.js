baef48b22f875de9fc25f47fe77a5acfd12e34f7
Chii Yuen <chii.yuen@hotmail.com>
2022-09-02 19:31:31 +0800

    Change event trigger

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let mode = "normal", size = 16, currentColor = '#000000', counter;
 const rgbPalette = ['#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800', '#FF5722', '#795548', '#9E9E9E', '#607D8B'];
+let mode = "normal", size = 16, currentColor = '#000000', mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
 function generateGrid() {
   const canvas = document.querySelector('.canvas');
@@ -17,39 +19,13 @@ function generateGrid() {
   cursor();
 }
 
-function cursor() {
-  const area = document.querySelectorAll('.area');
-  area.forEach(box => {
-    box.counter = 0;
-    box.addEventListener('mouseenter', (e) => {
-      switch (mode) {
-        case 'normal':
-          currentColor = e.target.style.backgroundColor = currentColor;
-          break;
-        case 'fill':
-          area.forEach(box => box.style.backgroundColor = currentColor);
-          break;
-        case 'rgb':
-          e.target.style.backgroundColor = rgbPalette[Math.round(Math.random() * rgbPalette.length)];
-          break;
-        case 'shade':
-          currentColor = '#000000';
-          box.counter += 1;
-          e.target.style.backgroundColor = currentColor;
-          e.target.style.opacity = 0.025 * box.counter;
-      }
-    })
-  })
-}
-
 function buttonListener() {
   const button = document.querySelectorAll('button, input');
   button.forEach(btn => {
     btn.addEventListener('click', () => {
       switch (btn.getAttribute('class')) {
         case 'erase':
-          mode = 'normal';
-          currentColor = '#ffffff';
+          mode = 'erase';
           break;
         case 'fill':
           mode = 'fill';
@@ -65,6 +41,7 @@ function buttonListener() {
           break;
         case 'clear':
           clear();
+          mode = 'normal';
           break;
         case 'resize':
           size = prompt("Enter a new grid size. You may choose from 16 to 100.");
@@ -73,6 +50,7 @@ function buttonListener() {
           }
           else {
             clear();
+            mode = 'normal';
             generateGrid();
             cursor();
           }
@@ -83,6 +61,37 @@ function buttonListener() {
   const color = document.querySelector('.color');
   color.addEventListener('change', (event) => {
     currentColor = event.target.value;
+  })
+}
+
+function cursor() {
+  const area = document.querySelectorAll('.area');
+  area.forEach(box => {
+    box.counter = 0;
+    box.addEventListener('mouseover', (e) => {
+      if (mouseDown) {
+        switch (mode) {
+          case 'erase':
+            e.target.style.backgroundColor = '#ffffff';
+            e.target.style.opacity = 1;
+            break;
+          case 'normal':
+            currentColor = e.target.style.backgroundColor = currentColor;
+            break;
+          case 'fill':
+            area.forEach(box => box.style.backgroundColor = currentColor);
+            break;
+          case 'rgb':
+            e.target.style.backgroundColor = rgbPalette[Math.round(Math.random() * rgbPalette.length)];
+            break;
+          case 'shade':
+            currentColor = '#000000';
+            box.counter += 1;
+            e.target.style.backgroundColor = currentColor;
+            e.target.style.opacity = 0.025 * box.counter;
+        }
+      }
+    })
   })
 }
 
