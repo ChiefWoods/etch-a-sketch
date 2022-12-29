5330e0d6aee821fa6078f18e9b63a27cdeade9d4
Chii Yuen <chii.yuen@hotmail.com>
2022-12-29 15:02:47 +0800

    Add footer, improve interface and script
    Rearrange and improve interface buttons
    
    Add slider for resizing
    
    Resize interface container
    
    Improve script

--- script.js
+++ script.js
@@ -1,120 +1,109 @@
-const rgbPalette = ['#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800', '#FF5722', '#795548', '#9E9E9E', '#607D8B'];
-let mode = "normal", size = 16, currentColor = '#000000', mouseDown = false;
-document.body.onmousedown = (e) => {
-  mouseDown = true;
+var selectedColor = '#000000', mode = 'normal', isMouseDown = false;
+
+const buttons = document.querySelectorAll('.mode');
+const canvas = document.querySelector('.canvas');
+const color = document.querySelector('input[type="color"]');
+const fill = document.querySelector('button[value="fill"]');
+const clear = document.querySelector('button[value="clear"]');
+const size = document.querySelector('.size');
+const range = document.querySelector('input[type="range"]');
+
+canvas.onmousedown = (e) => {
+  isMouseDown = true;
   e.preventDefault();
 }
-document.body.onmouseup = () => (mouseDown = false)
 
-function generateGrid() {
-  const canvas = document.querySelector('.canvas');
+canvas.onmouseup = () => {
+  isMouseDown = false;
+}
+
+color.addEventListener('change', () => {
+  selectedColor = color.value;
+})
+
+buttons.forEach(button => {
+  button.addEventListener('click', e => {
+    buttons.forEach(button => {
+      button.classList.remove('selected');
+    });
+    e.target.classList.add('selected');
+    mode = e.target.value;  
+  });
+})
+
+fill.addEventListener('click', () => {
+  canvas.childNodes.forEach(box => {
+    box.style.backgroundColor = selectedColor;
+    box.style.opacity = 1;
+  });
+  reset();
+})
+
+clear.addEventListener('click', () => {
+  canvas.childNodes.forEach(box => {
+    box.style.backgroundColor = '#ffffff';
+    box.style.opacity = 1;
+  });
+  reset();
+})
+
+range.addEventListener('input', () => {
+  size.textContent = `${range.value}`;
+})
+
+range.addEventListener('change', () => {
+  generateGrid(Number(range.value));
+  reset();
+})
+
+canvas.addEventListener('mouseover', e => {
+  if (!isMouseDown) return;
+  let box = e.target;
+  switch (mode) {
+    case 'normal':
+      box.style.backgroundColor = selectedColor;
+      box.style.opacity = 1;    
+      break;
+    case 'shade':
+      box.counter += 1;
+      box.style.backgroundColor = selectedColor;
+      box.style.opacity = 0.1 * box.counter;    
+      break;
+    case 'rgb':
+      box.style.backgroundColor = `rgb(${randomizer()}, ${randomizer()}, ${randomizer()})`;
+      box.style.opacity = 1;    
+      break;
+    case 'erase':
+      box.style.backgroundColor = '#ffffff';
+      box.style.opacity = 1;    
+      break;
+  }
+})
+
+function generateGrid(length) {
   while (canvas.firstChild) {
     canvas.firstChild.remove();
   }
-  canvas.style.setProperty('--columnNum', size);
-  canvas.style.setProperty('--rowNum', size);
-  for (let i = 0; i < size * size; i++) {
+  canvas.style.setProperty('grid-template-columns', 'repeat(' + length + ', 1fr)');
+  canvas.style.setProperty('grid-template-rows', 'repeat(' + length + ', 1fr)');
+  for (let i = 0; i < length * length; i++) {
     const square = document.createElement('div');
-    square.setAttribute('class', 'area');
     square.style.backgroundColor = '#ffffff';
+    square.style.opacity = 1;
+    square.counter = 0;
     canvas.appendChild(square);
   };
-  const sizeDisplay = document.querySelector('.size');
-  sizeDisplay.textContent = `Size : ${size}`; 
-  cursor();
 }
 
-function buttonListener() {
-  const button = document.querySelectorAll('button, input');
-  button.forEach(btn => {
-    btn.addEventListener('click', () => {
-      switch (btn.getAttribute('class')) {
-        case 'erase':
-          mode = 'erase';
-          break;
-        case 'fill':
-          mode = 'fill';
-          break;
-        case 'color':
-          mode = 'normal';
-          break;
-        case 'rgb':
-          mode = 'rgb';
-          break;
-        case 'shade':
-          mode = 'shade';
-          break;
-        case 'clear':
-          clear();
-          mode = 'normal';
-          break;
-        case 'resize':
-          size = prompt("Enter a new grid size. You may choose from 16 to 100.");
-          if (size < 16 || size > 100 || size == null) {
-            alert("Invalid grid size!");
-          }
-          else {
-            clear();
-            mode = 'normal';
-            generateGrid();
-            cursor();
-          }
-          break;
-      }
-    })
-  })
-  const color = document.querySelector('.color');
-  color.addEventListener('change', (event) => {
-    currentColor = event.target.value;
-  })
+function randomizer() {
+  return Math.floor(Math.random() * 255);
 }
 
-function cursor() {
-  const area = document.querySelectorAll('.area');
-  area.forEach(box => {
-    box.counter = 0;
-    box.addEventListener('mouseover', (e) => {
-      if (!mouseDown) return;
-      switch (mode) {
-        case 'erase':
-          e.target.style.backgroundColor = '#ffffff';
-          e.target.style.opacity = 1;
-          break;
-        case 'normal':
-          e.target.style.backgroundColor = currentColor;
-          e.target.style.opacity = 1;
-          break;
-        case 'fill':
-          area.forEach(box => {
-            box.style.backgroundColor = currentColor;
-            box.style.opacity = 1;
-          });
-          break;
-        case 'rgb':
-          e.target.style.backgroundColor = rgbPalette[Math.round(Math.random() * rgbPalette.length)];
-          break;
-        case 'shade':
-          box.counter += 1;
-          e.target.style.backgroundColor = currentColor;
-          e.target.style.opacity = 0.02 * box.counter;
-          break;
-      }
-    })
-  })
-}
-
-function clear() {
-  const square = document.querySelectorAll('.area');
-  square.forEach(sqr => {
-    sqr.style.backgroundColor = '#ffffff';
-    sqr.style.opacity = 1;
+function reset() {
+  buttons.forEach(button => {
+    button.classList.remove('selected');
   });
+  mode = "";
 }
 
-function runEtch() {
-  generateGrid();
-  cursor();
-  buttonListener();
-}
-
-runEtch();
+generateGrid(32);
